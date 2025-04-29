387cdd869d3559a0758810a0359616219c62fd76
Nadia Miroslava <nadiamrslv@gmail.com>
2025-04-29 10:05:38 -0600

    filters size

--- src/features/warehouse-master-detail/components/WarehouseFilters.tsx
+++ src/features/warehouse-master-detail/components/WarehouseFilters.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import FilterContainer from '../../../shared/components/filters/filter-container/FilterContainer';
-import FilterInput from '../../../shared/components/filters/filter-input/FilterInput';
+import FilterInput from '../../../shared/components/filters/filter-input/FilterInput'; 
 import { useWarehouseTableStore } from '../store/warehouseTableStore';
 import { usePaginatedWarehouseQuery } from '../hooks/usePaginatedWarehouseQuery';
 import WarehouseFiltersSkeleton from './WarehouseFiltersSkeleton';
@@ -14,98 +14,76 @@ export default function WarehouseFilters() {
   }
 
   return (
-    <FilterContainer onApply={() => setFilters(filters)} onReset={resetFilters}>
-
-          <FilterInput
-            label="Bodega"
-            type="autocomplete"
-            freeSolo
-            options={filterOptions.bodegas ?? []}
-            value={filters.bodega ?? ''}
-            onChange={(value) => setFilters({ ...filters, bodega: value })}
-          />
-
-          <FilterInput
-            label="Zona"
-            type="autocomplete"
-            freeSolo
-            options={filterOptions.zonas ?? []}
-            value={filters.zona ?? ''}
-            onChange={(value) => setFilters({ ...filters, zona: value })}
-          />
-
-          <FilterInput
-            label="Región"
-            type="autocomplete"
-            freeSolo
-            options={filterOptions.regiones ?? []}
-            value={filters.region ?? ''}
-            onChange={(value) => setFilters({ ...filters, region: value })}
-          />
-
-          <FilterInput
-            label="Clasificación"
-            type="autocomplete"
-            freeSolo
-            options={filterOptions.clasificaciones ?? []}
-            value={filters.clasificacion ?? ''}
-            onChange={(value) => setFilters({ ...filters, clasificacion: value })}
-          />
-
-          <FilterInput
-            label="Tipo de Ruta"
-            type="autocomplete"
-            freeSolo
-            options={filterOptions.tiposRuta ?? []}
-            value={filters.tipoRuta ?? ''}
-            onChange={(value) => setFilters({ ...filters, tipoRuta: value })}
-          />
-
-          <FilterInput
-            label="SKU"
-            type="autocomplete"
-            freeSolo
-            options={filterOptions.skus ?? []}
-            value={filters.sku ?? ''}
-            onChange={(value) => setFilters({ ...filters, sku: value })}
-          />
-
-          <FilterInput
-            label="Origen"
-            type="autocomplete"
-            freeSolo
-            options={filterOptions.bds ?? []}
-            value={filters.bd ?? ''}
-            onChange={(value) => setFilters({ ...filters, bd: value })}
-          />
-
-          <FilterInput
-            label="Estatus OPM"
-            type="autocomplete"
-            freeSolo
-            options={filterOptions.estatusOpm ?? []}
-            value={filters.estatusOpm ?? ''}
-            onChange={(value) => setFilters({ ...filters, estatusOpm: value })}
-          />
-
-          <FilterInput
-            label="Estatus SIO"
-            type="autocomplete"
-            freeSolo
-            options={filterOptions.estatusSio ?? []}
-            value={filters.estatusSio ?? ''}
-            onChange={(value) => setFilters({ ...filters, estatusSio: value })}
-          />
-
-          <FilterInput
-            label="Fecha de Registro"
-            type="autocomplete"
-            freeSolo
-            options={filterOptions.fechas ?? []}
-            value={filters.fechaRegistro ?? ''}
-            onChange={(value) => setFilters({ ...filters, fechaRegistro: value })}
-          />
-
+    <FilterContainer
+      onApply={() => setFilters(filters)}
+      onReset={resetFilters}
+    >
+      <FilterInput
+        label="Bodega"
+        type="autocomplete"
+        options={filterOptions.bodegas ?? []}
+        value={filters.bodega ?? ''}
+        onChange={(value) => setFilters({ ...filters, bodega: value })}
+      />
+      <FilterInput
+        label="Zona"
+        type="autocomplete"
+        options={filterOptions.zonas ?? []}
+        value={filters.zona ?? ''}
+        onChange={(value) => setFilters({ ...filters, zona: value })}
+      />
+      <FilterInput
+        label="Región"
+        type="autocomplete"
+        options={filterOptions.regiones ?? []}
+        value={filters.region ?? ''}
+        onChange={(value) => setFilters({ ...filters, region: value })}
+      />
+      <FilterInput
+        label="Clasificación"
+        type="autocomplete"
+        options={filterOptions.clasificaciones ?? []}
+        value={filters.clasificacion ?? ''}
+        onChange={(value) => setFilters({ ...filters, clasificacion: value })}
+      />
+      <FilterInput
+        label="Tipo de Ruta"
+        type="autocomplete"
+        options={filterOptions.tiposRuta ?? []}
+        value={filters.tipoRuta ?? ''}
+        onChange={(value) => setFilters({ ...filters, tipoRuta: value })}
+      />
+       <FilterInput
+        label="SKU"
+        type="autocomplete"
+        options={filterOptions.skus ?? []}
+        value={filters.sku ?? ''}
+      onChange={(value) => setFilters({ ...filters, sku: value })}
+      />
+      <FilterInput
+        label="Origen"
+        type="text"
+        value={filters.bd ?? ''}
+        onChange={(value) => setFilters({ ...filters, bd: value })}
+      />
+      <FilterInput
+        label="Estatus OPM"
+        type="text"
+        value={filters.estatusOpm ?? ''}
+        onChange={(value) => setFilters({ ...filters, estatusOpm: value })}
+      />
+      <FilterInput
+        label="Estatus SIO"
+        type="text"
+        value={filters.estatusSio ?? ''}
+        onChange={(value) => setFilters({ ...filters, estatusSio: value })}
+      />
+      <FilterInput
+        label="Fecha de Registro"
+        type="text" // podrías cambiar a date-picker si usas MUI X
+        value={filters.fechaRegistro ?? ''}
+        onChange={(value) => setFilters({ ...filters, fechaRegistro: value })}
+      />
     </FilterContainer>
   );
 }
