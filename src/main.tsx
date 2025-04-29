3c5e24aeb003fd6d18692bfd2e375696a31107e4
Nadia Miroslava <nadiamrslv@gmail.com>
2025-04-29 12:26:43 -0600

    fixes in use

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { router } from './routes/router.ts'
+import { router } from './routes/router.tsx'
 import { RouterProvider } from '@tanstack/react-router'
 
 createRoot(document.getElementById('root')!).render(
